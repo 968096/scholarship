90e09d5720bfffe6650bb6446c83fcbf2b3a3ca9
a53833 <a53833@alunos.ipb.pt>
2025-03-21 20:06:04 +0000

    update

--- backend/src/main/java/com/example/scholarshipdashboard/model/Scholarship.java
+++ backend/src/main/java/com/example/scholarshipdashboard/model/Scholarship.java
@@ -1,6 +1,14 @@
 package com.example.scholarshipdashboard.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Scholarship {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
--- backend/src/main/java/com/example/scholarshipdashboard/service/DashboardService.java
+++ backend/src/main/java/com/example/scholarshipdashboard/service/DashboardService.java
@@ -1,19 +1,20 @@
 package com.example.scholarshipdashboard.service;
 
 import com.example.scholarshipdashboard.model.Scholarship;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Service
 public class DashboardService {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     public List<Scholarship> getAllScholarships() {
-        return Arrays.asList(
-            new Scholarship(null, "Scholarship 1", "Description 1", 1000.0),
-            new Scholarship(null, "Scholarship 2", "Description 2", 2000.0),
-            new Scholarship(null, "Scholarship 3", "Description 3", 3000.0)
-        );
+        return entityManager.createQuery("SELECT s FROM Scholarship s", Scholarship.class).getResultList();
     }
 }
