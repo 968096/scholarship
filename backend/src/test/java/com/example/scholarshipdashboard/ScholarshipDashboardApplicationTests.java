319c59f5600d3d34c5b5ad3e6c6cbec870ad9c3c
a53833 <a53833@alunos.ipb.pt>
2025-03-21 19:17:51 +0000

    update

--- backend/src/test/java/com/example/scholarshipdashboard/ScholarshipDashboardApplicationTests.java
+++ backend/src/test/java/com/example/scholarshipdashboard/ScholarshipDashboardApplicationTests.java
@@ -3,11 +3,18 @@ package com.example.scholarshipdashboard;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = ScholarshipDashboardApplication.class) // Adicione esta linha
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=password",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class ScholarshipDashboardApplicationTests {
 
     @Test
     void contextLoads() {
+        // Test if the application context loads successfully
     }
 
 }
